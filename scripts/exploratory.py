2d0737e951f0eacbebb12a3a54068a3fe78f33bf
kingos82 <kingos82@gmail.com>
2022-10-02 22:01:59 -0700

    added cscat and cbox

--- scripts/exploratory.py
+++ scripts/exploratory.py
@@ -24,6 +24,26 @@ def cscat(p1: str, p2: str):
         color="class")
     return fig
 
+
+
+def cbox(p1: str): 
+    """
+    choose box
+    this function generates a box plot of variable p1 
+
+    p1 is the first parameter 
+    
+    output box plot
+    """
+
+    fig=px.plot(data_frame=df0,
+        x=p1,
+        y=p2,
+        kind='box',
+        title=p1+' vs '+p2, #f"{p1} vs {p2}"
+        color="class")
+    return fig
+
 if __name__ == '__main__':
  
     #Load iris data set into main program
@@ -38,4 +58,26 @@ if __name__ == '__main__':
 
     df0['class']=df0['class'].map(map_dic)
 
+    try:
+        
+        p=iris.feature_names
+        p0=''
+        for x in p:
+            p0+=x+', '
+
+        p1=input('Enter 1 of 4 parameters: '+p0)
+        
+        p.remove(p1)
+
+        p0=''
+        for x in p:
+            p0+=x+', '
+        p2=input('Enter 1 of 3 parameters: '+p0)
+
+        f0=cscat(p1, p2)
+        f1=cbox(p1)
+
+
+    except:
+        print('wrong input')
     
